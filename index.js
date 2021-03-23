65a2b01d45a87068f268882b7aeaec6a3938b44e
Antoine <antoine.famibelle@epitech.eu>
2021-03-23 20:26:37 +0100

    [ADDED] The color, the format and the logger in file [TODO] Refactor the Timestamp

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-function nationalogger(options) {
-  console.log('options : ', options);
-}
+const winston = require('winston');
 
-module.exports.nationalogger = nationalogger;
+const customFormat = winston.format.printf((args) => {
+  const { timestamp, level, message } = args;
+
+  return `${timestamp} | ${level}: ${message}`;
+});
+
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    customFormat,
+  ),
+  transports: [
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/all.log' }),
+  ],
+});
+
+logger.add(new winston.transports.Console());
+
+module.exports.logger = logger;
